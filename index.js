fa469a7397b527a42c71cb2ddbc22c4ac82742f7
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-04-29 00:54:37 +0600

    .env

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-// exploreMaster
-// HL9nh4rd9tut5bje
 
+console.log(process.env.DB_USER, process.env.DB_PASSWORD);
 
-const uri = "mongodb+srv://exploreMaster:HL9nh4rd9tut5bje@cluster0.t8njxkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t8njxkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
