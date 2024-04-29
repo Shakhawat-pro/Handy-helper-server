4f4f635e2cd1894d9990171ae4d896641c2810c2
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-04-29 21:23:38 +0600

    Country params get

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ async function run() {
         const result = await cursor.toArray()
         res.send(result)
     })
+    app.get('/country/:id', async(req, res) => {
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await countryCollection.findOne(query)
+        res.send(result)
+    })
     app.get('/spot/:id', async(req, res) => {
         const id = req.params.id
         const query = {_id: new ObjectId(id)}
