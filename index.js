69982011aede43929ccf377f0a27d332963aa7df
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-04-29 12:42:55 +0600

    DB delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,13 @@ async function run() {
         const newSpot = req.body;
         console.log(newSpot);
         const result = await spotCollection.insertOne(newSpot)
+        res.send(result)
+    })
+
+    app.delete('/spot/:id', async(req, res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await spotCollection.deleteOne(query)
         res.send(result)
     })
 
